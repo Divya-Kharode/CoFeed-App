3a050ffb29f6a97ad1b92518091a482ce85145b0
Divya Kharode <kharodedm19.comp@coep.ac.in>
2020-05-25 06:24:19 +0530

    CoFeed Components

---
+++ config/rest_config.js
@@ -0,0 +1,4 @@
+export const articles_url = 'http://newsapi.org/v2/top-headlines';
+export const country_code = 'in';
+export const category = 'health';
+export const _api_key_ = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
