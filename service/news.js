d2cfb08493df173b63ce3deac21db1db3dcb5070
Divya Kharode <65601624+Divya-Kharode@users.noreply.github.com>
2020-05-25 21:02:19 +0530

    Final Commit

--- service/news.js
+++ service/news.js
@@ -3,7 +3,7 @@ import { articles_url, _api_key, country_code } from '../config/rest_config';
 export async function getArticles(category='health') {
 
     try {
-        let articles = await fetch("http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=10603319f85d4bf0a30da7bb73e93d96");
+        let articles = await fetch("http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=1XXXXXXXXXXXX6");
 
         let result = await articles.json();
         articles = null;
